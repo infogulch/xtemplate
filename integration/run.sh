fcdf7c9282be67c6929b0fe0fb8f459c5b417001
infogulch <infogulch@gmail.com>
2023-10-16 00:30:23 -0500

    Don't build caddy yet

--- integration/run.sh
+++ integration/run.sh
@@ -5,10 +5,10 @@ echo "Running xtemplate..."
 pushd `dirname "$(readlink -f "$0")"` > /dev/null    # cd to the directory where this script is
 go run ../bin -log -4 > xtemplate.log &              # exec go run in the background
 PID=$!                                               # grab the pid
-exit() {         # define exit handler
-    sleep 0.1s   # wait for stdout to flush
-    kill $PID    # kill the process
-    popd         # cd back to wherever the script was invoked from
+exit() {             # define exit handler
+    sleep 0.1s       # wait for stdout to flush
+    kill $PID        # kill the process
+    popd > /dev/null # cd back to wherever the script was invoked from
 }
 trap exit EXIT   # register exit handler
 until grep -q -i 'msg=serving' xtemplate.log  # wait for the server to start
