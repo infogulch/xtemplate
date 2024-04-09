3716926db7fd947270d8271de43e2dc8249783a1
infogulch <infogulch@gmail.com>
2024-04-08 20:02:12 -0500

    Add nats request

--- providers/nats/nats.go
+++ providers/nats/nats.go
@@ -2,6 +2,8 @@ package nats
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/nats-io/nats.go"
 )
@@ -28,4 +30,18 @@ func (d *DotNats) Subscribe(subject string) (<-chan *nats.Msg, error) {
 
 func (d *DotNats) Publish(subject, message string) error {
 	return d.conn.Publish(subject, []byte(message))
+}
+
+func (d *DotNats) Request(subject, data string, timeout_ ...time.Duration) (*nats.Msg, error) {
+	var timeout time.Duration
+	switch len(timeout_) {
+	case 0:
+		timeout = 1 * time.Second
+	case 1:
+		timeout = timeout_[0]
+	default:
+		return nil, fmt.Errorf("too many timeout args")
+	}
+
+	return d.conn.Request(subject, []byte(data), timeout)
 }
